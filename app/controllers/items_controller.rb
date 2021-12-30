05725fa245354fd852c199981e66b6128c4916ca
kazuhiro-tsuchiya <nagoya758eight10@gmail.com>
2021-12-30 13:00:25 +0900

    javascriptの記述、ActiveHashでのカテゴリー選択の記述

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,6 +2,7 @@ class ItemsController < ApplicationController
   before_action :move_to_signed_in, except: [:index]
 
   def index
+    @items = Item.order("created_at DESC")
     @items = Item.all
   end
 
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,14 +1,21 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :quality
+  belongs_to :burden
+  belongs_to :area
+  belongs_to :delivery_day
   has_one_attached :image
 
   validates :item_name,       presence: true
   validates :item_explain,    presence: true
-  validates :category_id,     presence: true
-  validates :quality_id,      presence: true
-  validates :burden_id,       presence: true
-  validates :area_id,         presence: true
-  validates :delivery_day_id, presence: true
-  validates :price,           presence: true
+  validates :category_id,     presence: true, numericality: { other_than: 1, message: "can't be blank"}
+  validates :quality_id,      presence: true, numericality: { other_than: 1, message: "can't be blank"}
+  validates :burden_id,       presence: true, numericality: { other_than: 1, message: "can't be blank"}
+  validates :area_id,         presence: true, numericality: { other_than: 1, message: "can't be blank"}
+  validates :delivery_day_id, presence: true, numericality: { other_than: 1, message: "can't be blank"}
+  validates :price,           presence: true, numericality: { with: /\A[0-9]+\z/, message: 'Half-width number' }
+  validates :price,           numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999, message: 'Out of setting range' }
   validates :user_id,         presence: true
 
   validates :image,           presence: true
