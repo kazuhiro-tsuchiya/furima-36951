77858790bda0278e55122d467ded775ba45bd316
kazuhiro-tsuchiya <nagoya758eight10@gmail.com>
2022-01-12 15:20:34 +0900

    addressモデルのバリデーションの設定

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,3 +1,11 @@
 class Address < ApplicationRecord
   belongs_to :purchase
+
+  with_options presence: true do
+    validates :post_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+    validates :area_id,   numericality: {other_than: 0, message: "can't be blank"}
+    validates :city
+    validates :street
+    validates :phone_number      
+  end
 end
