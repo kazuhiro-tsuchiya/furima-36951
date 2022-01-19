52033a3ea8085bcdbb1111d7d5c1fbe1f9d49c93
kazuhiro-tsuchiya <nagoya758eight10@gmail.com>
2022-01-19 16:32:45 +0900

    herokuでのエラー

--- app/models/purchase_address.rb
+++ app/models/purchase_address.rb
@@ -1,6 +1,6 @@
 class PurchaseAddress
   include ActiveModel::Model
-  attr_accessor :item_id, :user_id, :post_code, :area_id, :city, :street, :building_name, :phone_number, :token
+  attr_accessor :item_id, :user_id, :post_code, :area_id, :city, :street, :building_name, :phone_number, :token, :price
 
   with_options presence: true do
     validates :user_id
